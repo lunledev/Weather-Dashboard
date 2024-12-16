459b2c437bbed5a1fbfbee8b1979ea60dfc1d4f6
lunledev <183235731+lunledev@users.noreply.github.com>
2024-12-16 14:58:18 -0800

    updated weatherRoutes.ts file

--- server/src/routes/api/weatherRoutes.ts
+++ server/src/routes/api/weatherRoutes.ts
@@ -17,13 +17,49 @@ router.post('/', (req, res) => {
     res.send('error in adding city');
   }
 
-  }
 });
 
 // TODO: GET search history
-router.get('/history', async (req, res) => {});
+router.get('/history', async (_req, res) => {
+try{
+
+  const savedcityname = await HistoryService.getCities();
+  res.json(savedcityname);
+  
+}
+catch(err) {
+  console.log(err);
+  res.status(500).json(err);
+
+}
+  
+
+});
 
 // * BONUS TODO: DELETE city from search history
-router.delete('/history/:id', async (req, res) => {});
+router.delete('/history/:id', async (req, res) => {
+try
+{
+  if(!req.params.id) {
+
+    res.status(400).json(
+      { message: 'city id is required'}
+      
+      );
+      await HistoryService.deleteCity(req.params.id);
+      res.json({success:'city successfully removed from search history'});
+  }
+
+}
+catch(err) {
+
+  console.log(err); 
+  res.status(500).json(err);
+
+
+}
+
+
+});
 
 export default router;
